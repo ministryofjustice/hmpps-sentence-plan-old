c37d21a737db9feb02d543fb6de041cbffe6e0f3
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-04-13 15:00:27 +0100

    PI-986

---
+++ build.gradle.kts
@@ -0,0 +1,105 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.3"
+  kotlin("jvm") version "1.8.20"
+  kotlin("plugin.spring") version "1.8.20"
+  kotlin("plugin.jpa") version "1.8.20"
+  id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+  id("com.google.cloud.tools.jib") version "3.3.1"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+}
+
+extra["springCloudVersion"] = "2022.0.2"
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  // implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  // implementation("org.flywaydb:flyway-core")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+  implementation("io.sentry:sentry-spring-boot-starter:6.17.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+  // runtimeOnly("org.postgresql:postgresql")
+
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.security:spring-security-test")
+}
+
+dependencyManagement {
+  imports {
+    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+  }
+}
+
+tasks {
+  val copyAgentJar by registering(Copy::class) {
+    from("${project.buildDir}/libs")
+    include("applicationinsights-agent*.jar")
+    into("${project.buildDir}/agent")
+    rename("applicationinsights-agent(.+).jar", "agent.jar")
+    dependsOn("assemble")
+  }
+
+  val jib by getting {
+    dependsOn += copyAgentJar
+  }
+
+  val jibBuildTar by getting {
+    dependsOn += copyAgentJar
+  }
+
+  val jibDockerBuild by getting {
+    dependsOn += copyAgentJar
+  }
+
+  withType<KotlinCompile> {
+    kotlinOptions {
+      freeCompilerArgs = listOf("-Xjsr305=strict")
+      jvmTarget = "17"
+    }
+  }
+
+  withType<Test> {
+    useJUnitPlatform()
+  }
+}
+
+jib {
+  container {
+    jvmFlags = mutableListOf("-Duser.timezone=Europe/London")
+    mainClass = "uk.gov.justice.digital.hmpps.sentenceplan.SentencePlanApplicationKt"
+    user = "2000:2000"
+  }
+  from {
+    image = "eclipse-temurin:17-jre-jammy"
+  }
+  extraDirectories {
+    paths {
+      path {
+        setFrom("${project.buildDir}")
+        includes.add("agent/agent.jar")
+      }
+      path {
+        setFrom("${project.rootDir}")
+        includes.add("applicationinsights*.json")
+        into = "/agent"
+      }
+    }
+  }
+}
