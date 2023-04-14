6715fe18951fccc46ff2e15231919538a14bb042
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-04-14 12:33:51 +0100

    initial DB access

---
+++ src/main/resources/db/migration/V0__init.sql
@@ -0,0 +1,8 @@
+create table if not exists person
+(
+    id      uuid primary key,
+    crn     varchar null,
+    noms_id varchar null,
+    constraint person_crn unique (crn),
+    constraint person_noms_id unique (noms_id)
+)
