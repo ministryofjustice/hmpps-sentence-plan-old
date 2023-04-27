6920c58fb515593dace863ce248ecaa4d3aeae01
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-04-27 10:40:51 +0100

    updated url structure

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/model/SentencePlan.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/model/SentencePlan.kt
@@ -6,4 +6,5 @@ import java.util.UUID
 data class SentencePlan(
   val createdDate: ZonedDateTime,
   val id: UUID?,
+  val crn: String,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/resource/SentencePlanResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/resource/SentencePlanResource.kt
@@ -4,7 +4,6 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.sentenceplan.model.SentencePlan
 import uk.gov.justice.digital.hmpps.sentenceplan.service.SentencePlanService
@@ -15,6 +14,6 @@ class SentencePlanResource(private val service: SentencePlanService) {
 
   @PreAuthorize("hasRole('ROLE_SENTENCE_PLAN_RW')")
   @PostMapping
-  fun createSentencePlan(@RequestParam crn: String, @RequestBody sentencePlan: SentencePlan): SentencePlan =
-    service.createSentencePlan(crn, sentencePlan)
+  fun createSentencePlan(@RequestBody sentencePlan: SentencePlan): SentencePlan =
+    service.createSentencePlan(sentencePlan)
 }
