31c74ef521291a6046f93d718060e54c90774d4a
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-08-31 10:58:22 +0100

    PI-1419 combined updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.0"
-  kotlin("jvm") version "1.8.20"
-  kotlin("plugin.spring") version "1.8.20"
-  kotlin("plugin.jpa") version "1.9.0"
+  kotlin("jvm") version "1.9.10"
+  kotlin("plugin.spring") version "1.9.10"
+  kotlin("plugin.jpa") version "1.9.10"
   id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
   id("com.google.cloud.tools.jib") version "3.3.2"
   jacoco
